ba6fbba890eaba27bb78eeaf06dda97aa8f8b08d
gesishqau-prject <gesi.shqau@gmail.com>
2026-09-24 16:35:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ function detectIntent(text) {
   if (/\b(can.?t breathe|cant breathe|difficulty breathing|trouble breathing|difficulty swallowing|trouble swallowing|won.?t stop bleeding|wont stop bleeding|uncontrolled bleeding|heavy bleeding|knocked out|severe swelling|facial swelling|face is swollen|high fever|passed out|unconscious)\b/.test(t)) {
     return 'emergency';
 }
-  }
+
   if (/\b(whiten|whitening)\b/.test(t)) return 'whitening';
   if (/\b(invisalign|aligner|braces)\b/.test(t)) return 'invisalign';
   if (/\b(insurance|ppo|cover|coverage|plan)\b/.test(t)) return 'insurance';
